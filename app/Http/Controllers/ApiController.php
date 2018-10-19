469086be729179f580566dc8aaf41652305b2c92
Mohamed Refaie <mohamed1refaie@hotmail.com>
2018-10-19 16:53:12 +0200

    Login and register

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterAuthRequest;
+use App\User;
+use Illuminate\Http\Request;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class ApiController extends Controller
+{
+    public function register(RegisterAuthRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $lol = $user->save();
+        if(!$lol)return "no";
+        else return"sa7";
+        /*if($user->save())
+        {
+            return response()->json([
+                'success' => true,
+                'data' => $user
+            ], 201);
+        }
+        else
+        {
+            return response()->json([
+                'success' => false,
+                'error' => 'User cannot be created',
+            ],400);
+        }*/
+
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
+    }
+
+    public function getAuthUser(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        $user = JWTAuth::authenticate($request->token);
+
+        return response()->json(['user' => $user]);
+    }
+}
